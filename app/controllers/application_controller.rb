c57aa050d19c8ac914190896027aef7830111a66
EC2 Default User <ec2-user@ip-172-31-17-225.ap-northeast-1.compute.internal>
2023-05-31 09:30:58 +0900

    ユーザー機能の追加

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,11 @@
 class ApplicationController < ActionController::Base
+    before_action :configure_permitted_parameters, if: :devise_controller?
+
+  #新規登録時のストロングパラメータに「username, job, profile」カラムの追加
+  def configure_permitted_parameters
+      #新規登録時のストロングパラメータにカラムの追加
+      devise_parameter_sanitizer.permit(:sign_up, keys: [:user_name, :avatar, :is_admin])
+      #編集時のストロングパラメータにカラムの追加
+      devise_parameter_sanitizer.permit(:account_update, keys: [:user_name, :avatar, :is_admin])
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  devise_for :users
+  # get '/users/edit', to: 'users#edit'
+  devise_scope :user do
+    get '/users/sign_out' => 'devise/sessions#destroy'
+  end
 end
